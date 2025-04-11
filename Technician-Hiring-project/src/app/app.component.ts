f07a8eaf8216a76f8fee02cb0c908a09cadd5ceb
Sara-Qadi <sara.2002.qado@gmail.com>
2025-04-11 04:54:02 +0300

    first draft

--- Technician-Hiring-project/src/app/app.component.ts
+++ Technician-Hiring-project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationComponent } from './modules/notification/notification/notification.component';
+import { NotificationDropdownComponent } from './modules/notification/notification-dropdown/notification-dropdown.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotificationComponent, NotificationDropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Technician-Hiring-project/src/app/app.routes.ts
+++ Technician-Hiring-project/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NotificationComponent } from './modules/notification/notification/notification.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'notifications', component: NotificationComponent },
+];
