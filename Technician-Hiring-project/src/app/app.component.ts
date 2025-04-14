68b972b8beef3f36e479f3b3d860bd47c977ec45
Sara-Qadi <sara.2002.qado@gmail.com>
2025-04-14 16:25:54 +0300

    edit2 draft

--- Technician-Hiring-project/src/app/app.component.ts
+++ Technician-Hiring-project/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './modules/notification/notification/notification.component';
 import { NotificationDropdownComponent } from './modules/notification/notification-dropdown/notification-dropdown.component';
+import { ProfileComponent } from './modules/technician/profile/profile.component';
+import { ProfileEditComponent } from './modules/technician/profile-edit/profile-edit.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NotificationComponent, NotificationDropdownComponent],
+  imports: [RouterOutlet, NotificationComponent, NotificationDropdownComponent, ProfileComponent, ProfileEditComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
