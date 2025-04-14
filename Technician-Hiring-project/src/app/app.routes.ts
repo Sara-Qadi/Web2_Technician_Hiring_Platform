70b0b9c5d780f327e442437bd56c96b0db8feccf
Hamza Zarour <s12323435@stu.najah.edu>
2025-04-14 18:03:07 +0300

    the front end for log-in & forgot pass & role-selection & sign-up pages

--- Technician-Hiring-project/src/app/app.routes.ts
+++ Technician-Hiring-project/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './modules/auth/login-page/login-page.component';
+import { RoleSelectionComponent } from './modules/auth/role-selection/role-selection.component';
+import { SignUpComponent } from './modules/auth/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './modules/auth/forgot-password/forgot-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: '',
+    component: LoginPageComponent,
+  },
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full',
+  // },
+  { path: 'role-selection', component: RoleSelectionComponent }, // المسار لصفحة اختيار الدور
+
+  { path: 'sign-up', component: SignUpComponent },
+
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+];
+
