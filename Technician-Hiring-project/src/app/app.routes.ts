29d48ae932ccf950c09091eafc7ba4a2220acb61
Your Name <you@example.com>
2025-04-14 19:07:39 +0300

    Halanew

--- Technician-Hiring-project/src/app/app.routes.ts
+++ Technician-Hiring-project/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {
+  CraftsmenRegistrationsComponent
+} from './modules/admin/admin/craftsmen-registrations/craftsmen-registrations.component';
+import {AdminComponent} from './modules/admin/admin/admin.component';
+import {DeleteCraftsmanComponent} from './modules/admin/admin/delete-craftsman/delete-craftsman.component';
+import {JopListingComponent} from './modules/admin/admin/jop-listing/jop-listing.component';
+import {NavbarAdminComponent} from './modules/admin/admin/navbar-admin/navbar-admin.component';
+import {FooterAdminComponent} from './modules/admin/admin/footer-admin/footer-admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'admin/dashboard', component:AdminComponent},
+  {path: 'admin/craftsmen-registrations', component:CraftsmenRegistrationsComponent},
+  {path: 'admin/delete-craftsman', component:DeleteCraftsmanComponent},
+  {path: 'admin/jop-listing', component:JopListingComponent},
+  {path: 'navbar-admin', component: NavbarAdminComponent},
+  {path: 'footer-admin', component:FooterAdminComponent},
+
+];
--- Technician-Hiring-project/src/app/modules/admin/admin/admin.component.ts
+++ Technician-Hiring-project/src/app/modules/admin/admin/admin.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {NavbarAdminComponent} from './navbar-admin/navbar-admin.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [
+    NavbarAdminComponent,
+    RouterOutlet
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
