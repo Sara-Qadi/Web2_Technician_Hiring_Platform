73da607fdfff9622e16746ac6afa0b3d748e8d4b
Sara-Qadi <sara.2002.qado@gmail.com>
2025-05-31 00:44:22 +0300

    total subissions and total posts added for dashboard

---
+++ Technician-Hiring-project/src/app/services/dashboard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private baseUrl = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+getTotalPosts(): Observable<{ total_posts: number }> {
+  return this.http.get<{ total_posts: number }>(`${this.baseUrl}/dashboard/total-posts`);
+}
+
+
+
+  getTotalSubmissions(): Observable<{ total_submissions: number }> {
+    return this.http.get<{ total_submissions: number }>(`${this.baseUrl}/dashboard/total-submissions`);
+  }
+}
