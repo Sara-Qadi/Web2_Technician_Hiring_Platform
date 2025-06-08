abdfb60cf20c708afdd8b2ac484ba40b31a5f314
Sara-Qadi <sara.2002.qado@gmail.com>
2025-06-08 19:37:59 +0300

    edits

--- Technician-Hiring-project/src/app/services/dashboard.service.ts
+++ Technician-Hiring-project/src/app/services/dashboard.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -12,27 +11,24 @@ export class DashboardService {
 
   constructor(private http: HttpClient) {}
 
-getTotalPosts(): Observable<{ total_posts: number }> {
-  return this.http.get<{ total_posts: number }>(${this.baseUrl}/dashboard/total-posts);
-}
-
-
+  getTotalPosts(): Observable<{ total_posts: number }> {
+    return this.http.get<{ total_posts: number }>(`${this.baseUrl}/dashboard/total-posts`);
+  }
 
   getTotalSubmissions(): Observable<{ total_submissions: number }> {
-    return this.http.get<{ total_submissions: number }>(${this.baseUrl}/dashboard/total-submissions);
+    return this.http.get<{ total_submissions: number }>(`${this.baseUrl}/dashboard/total-submissions`);
   }
 
   getJobPostsByMonth(): Observable<any> {
-  return this.http.get(${this.baseUrl}/dashboard/jobposts-per-month);
-}
+    return this.http.get(`${this.baseUrl}/dashboard/jobposts-per-month`);
+  }
 
-getPendingApprovals(): Observable<number> {
-  return this.http.get<{ pending_approvals: number }>(${this.baseUrl}/users/pending-approvals)
-    .pipe(map(res => res.pending_approvals));
-}
+  getPendingApprovals(): Observable<number> {
+    return this.http.get<{ pending_approvals: number }>(`${this.baseUrl}/users/pending-approvals`)
+      .pipe(map(res => res.pending_approvals));
+  }
 
-getJobStatusCounts(): Observable<{ in_progress: number, completed: number }> {
-  return this.http.get<{ in_progress: number, completed: number }>(`${this.baseUrl}/dashboard/job-status-counts`);
-}
-
+  getJobStatusCounts(): Observable<{ in_progress: number, completed: number }> {
+    return this.http.get<{ in_progress: number, completed: number }>(`${this.baseUrl}/dashboard/job-status-counts`);
+  }
 }
